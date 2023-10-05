24fee0ab1ddd74e29b46598fe2d435f45d93dc28
aclaudiacarvalho <aclaudia_carvalho@outlook.com>
2023-10-04 22:57:21 -0300

    content creater

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake =[
+  {
+    "id":"1",
+    "title":"Novo Homen de Ferro anuciado!",
+    "description":"Estudio Marvel anuncia novo filme de homen de ferro para 2024",
+    "photoCover":"https://jovemnerd.com.br/wp-content/uploads/2022/09/imagem_2022_09_20_121613462__9sfi6086.png"
+  },
+  {
+    "id":"2",
+    "title":"Estreia nova serie Marvel.",
+    "description":"Olá",
+    "photoCover":"https://th.bing.com/th/id/OIP.bQfn3Pm9UaBJjftejo3gbgHaEK?w=331&h=186&c=7&r=0&o=5&pid=1.7"
+  },
+  {
+    "id":"3",
+    "title":"Mundo Marvel esta sob ataque!!!",
+    "description":"Olá",
+    "photoCover":"https://wallpapercave.com/dwp1x/wp1829345.jpg"
+  },
+  {
+    "id":"4",
+    "title":"Por onde anda os Vingadores ? ",
+    "description":"Olá",
+    "photoCover":"https://wallpapercave.com/dwp1x/MeoyARM.jpg"
+  }
+];
